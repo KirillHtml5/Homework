83c8a248ec85d43c6c997ac652cefc80ba655ae3
KirillHtml5 <mr.kiril17@icloud.com>
2022-05-02 17:22:29 +0300

    add h11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,11 +1,19 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import s from './common/c7-SuperRange/SuperRange.module.css'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const [value1, setValue1] = useState<number>(0)
+    const [value2, setValue2] = useState<number>(100)
+    let value3 = [value1, value2]
+    let onChangeRange = (value1: number, value2: number) => {
+        setValue1(value1)
+        setValue2(value2)
+    }
 
+    // @ts-ignore
+    // @ts-ignore
     return (
         <div>
             <hr/>
@@ -13,18 +21,20 @@ function HW11() {
 
             {/*should work (должно работать)*/}
             <div>
-                <span>{value1}</span>
-                <SuperRange
+                <span className={s.value}>{value1}</span>
+                <SuperRange value={value1} onChangeRange={(value: number) => {
+                    setValue1(value)
+                }}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
             <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
+                <span className={s.value}>{value1}</span>
+                <SuperDoubleRange value={[value1,value2]} onChangeRange={onChangeRange}
                     // сделать так чтоб value1 и value2 изменялось
                 />
-                <span>{value2}</span>
+                <span className={s.value}>{value2}</span>
             </div>
 
             <hr/>
